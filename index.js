ce0154ae3b98b593a96310bf3032a6b62b142016
VinitKasture <97528616+VinitKasture@users.noreply.github.com>
2024-02-28 21:21:34 +0530

    cors

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_SERVER_URL,
+  })
+);
 
 const todoRoutes = require("./routes/todos.route");
 const authRoutes = require("./routes/auth.route");
