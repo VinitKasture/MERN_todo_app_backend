856415b4da404c436efab9f00260921d91859cc3
VinitKasture <97528616+VinitKasture@users.noreply.github.com>
2024-02-28 18:17:23 +0530

    cors

--- index.js
+++ index.js
@@ -12,17 +12,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 app.use(todoRoutes);
 app.use(authRoutes);
